8f0be4f134b85c858670f382301ea2e0febab8ed
David Dias <170323009@estudantes.ips.pt>
2019-04-06 11:18:55 +0100

    Novas views e controlos

--- SetepassosPRJ/Controllers/JogoController.cs
+++ SetepassosPRJ/Controllers/JogoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SetepassosPRJ.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,39 @@ namespace SetepassosPRJ.Controllers
     {
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CriarJogo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CriarJogo(string nome)
+        {
+            if (ModelState.IsValid)
+            {
+                Perfil NovoPerfil = new Perfil
+                {
+                    Nome = nome,
+                    MoedasOuro = 0,
+                    PontosAtaque = 4,
+                    PontosSorte = 3,
+                    PontosVida = 2,
+                    PocoesVida = 1,
+                    Chave = false
+                };
+                return View("JogoIniciado", NovoPerfil);
+            }
+            else
+                return View();
+        }
+
+       
+        public IActionResult JogoMonstro()
         {
             return View();
         }
---
+++ SetepassosPRJ/Models/Perfil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetepassosPRJ.Models
+{
+    public class Perfil
+    {
+        private string nome;
+        private int moedasOuro;
+        private int pontosVida;
+        private int pontosAtaque;
+        private int pontosSorte;
+        private int pocoesVida;
+        private bool chave;
+
+        [Required(ErrorMessage ="Introduza um nome")]
+        public string Nome { get; set; }
+
+        public int MoedasOuro { get; set; }
+        public int PontosVida { get; set; }
+        public int PontosAtaque { get; set; }
+        public int PontosSorte { get; set; }
+        public int PocoesVida { get; set; }
+        public bool Chave { get; set; }
+
+        
+    }
+}
