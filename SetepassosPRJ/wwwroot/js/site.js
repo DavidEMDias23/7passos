f605de3154c1c74d8f81ce3549ee2b4700017c6d
David Dias <170323009@estudantes.ips.pt>
2019-04-18 14:19:52 +0100

    Remoção de código não utilizado e inserção de comentários

--- SetepassosPRJ/wwwroot/js/site.js
+++ SetepassosPRJ/wwwroot/js/site.js
@@ -3,10 +3,8 @@
 
 // Write your JavaScript code.
 
-//$('.carousel').carousel({
-  //interval: 200
-//});
 
+//Código para conforme se "avança" de slide no carousel aparecer texto e esse texto ser adequado a imagem que aparece
 $('#text').html($('.active > .carousel-caption').html());
 $('.carousel').on('slid.bs.carousel', function (e) {
   	$('#text').html($('.active > .carousel-caption').html());
