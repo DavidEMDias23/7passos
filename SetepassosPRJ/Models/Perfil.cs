645dc75a31643a762b31788b1faa484cf1f55379
David Dias <170323009@estudantes.ips.pt>
2019-05-09 18:20:22 +0100

    fixes #2 Inserção de uma nova propriedade Monstro no Modelo e nova condição para mostrar a view conforme exista monstro ou não.

--- SetepassosPRJ/Models/Perfil.cs
+++ SetepassosPRJ/Models/Perfil.cs
@@ -16,47 +16,50 @@ namespace SetepassosPRJ.Models
         public int PocoesVida { get; set; }
         public bool Chave { get; set; }
         public int Sala { get; set; }
-
+        public bool Monstro { get; set; }
+        public bool ItemSurpresa { get; set; }
 
         public Perfil(string nomeEscolhido, string perfilTipoEscolhido)
-        {  
+        {
+
+            MoedasOuro = 0;
+            Chave = false;
+            Sala = 1;
+            Monstro = false;
+            ItemSurpresa = false;
+
             if (perfilTipoEscolhido == "Nerd")
             {
                 Nome = nomeEscolhido;
-                PerfilTipo = perfilTipoEscolhido;
-                MoedasOuro = 0;
+                PerfilTipo = perfilTipoEscolhido;              
                 PontosVida = 4;
                 PontosAtaque = 3;
                 PontosSorte = 2;
                 PocoesVida = 1;
-                Chave = false;
-                Sala = 1;
+                
+
             }
 
             if (perfilTipoEscolhido == "Fat")
             {
                 Nome = nomeEscolhido;
                 PerfilTipo = perfilTipoEscolhido;
-                MoedasOuro = 0;
                 PontosVida = 3;
                 PontosAtaque = 3;
                 PontosSorte = 3;
                 PocoesVida = 1;
-                Chave = false;
-                Sala = 1;
+
             }
 
             if (perfilTipoEscolhido == "Old")
             {
                 Nome = nomeEscolhido;
                 PerfilTipo = perfilTipoEscolhido;
-                MoedasOuro = 0;
                 PontosVida = 3;
                 PontosAtaque = 2;
                 PontosSorte = 4;
                 PocoesVida = 1;
-                Chave = false;
-                Sala = 1;
+
             }
 
         }
