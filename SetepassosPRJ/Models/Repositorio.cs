baf43b4a3d341a64cea6f09bfe2f423287767510
David Dias <170323009@estudantes.ips.pt>
2019-05-14 17:25:53 +0100

    Criado modelo Repositorio (Listas)

---
+++ SetepassosPRJ/Models/Repositorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetepassosPRJ.Models
+{
+    public static class Repositorio
+    {
+        private static List<Perfil> perfil = new List<Perfil>();
+
+        public static List<Perfil> Perfil
+        {
+            get
+            {
+                return perfil;
+            }
+        }
+
+        public static void AddPerfil(Perfil perfil)
+        {
+            Perfil.Add(perfil);
+        }
+    }
+}
